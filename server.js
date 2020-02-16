5e5c03df0bb8f08612e94b5d442a3a0f552de088
William Y Wong <wwong1987@gmail.com>
2020-02-15 22:15:11 -0800

    connected to mongo db

--- server.js
+++ server.js
@@ -3,5 +3,11 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-app.listen(3000, () => console.log('server started'));
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('connected to database'));
+
+app.listen(3000, () => console.log('server started on http://localhost:3000'));

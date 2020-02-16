6e73d9c884eaca471c94a408bf0980f2965c8321
William Y Wong <wwong1987@gmail.com>
2020-02-16 00:43:39 -0800

    added middleware function for subscriber by ID

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -15,14 +15,13 @@ router.get('/', async (req, res) => {
   }
 });
 
-
 // Get one subscriber
-router.get('/:id', (req, res) => {});
-
+router.get('/:id', async (req, res) => {});
 
 // Create one subscriber
 router.post('/', async (req, res) => {
-  const subscriber = new Subscriber({ // creating a variable that will be assigned to a new Subscriber from our model created earlier (name, subscribedChannel, and subscribeDate)
+  const subscriber = new Subscriber({
+    // creating a variable that will be assigned to a new Subscriber from our model created earlier (name, subscribedChannel, and subscribeDate)
     name: req.body.name,
     subscribedChannel: req.body.subscribedChannel
   });
@@ -40,5 +39,19 @@ router.patch('/:id', (req, res) => {});
 
 // Delete one subscriber
 router.delete('/:id', (req, res) => {});
+
+// middleware function for getting subscriber obj by ID
+async function getSubscriber(req, res, next) {
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber === null) {
+      return res.status(404).json({ message: 'Subscriber not found' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+}
 
 module.exports = router;

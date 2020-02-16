684132a8f9e6a81c91f8d3991f747a0042b61a42
William Y Wong <wwong1987@gmail.com>
2020-02-16 00:23:42 -0800

    completed post route for creating one subscriber

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -7,19 +7,33 @@ const Subscriber = require('../models/subscriber');
 // Get all subscribers
 router.get('/', async (req, res) => {
   try {
-    const subscribers = await Subscriber.find()
+    const subscribers = await Subscriber.find();
     // console.log(subscribers)
-    res.json(subscribers)
+    res.json(subscribers);
   } catch (err) {
-    res.status(500).json({ message: err.message })
+    res.status(500).json({ message: err.message });
   }
 });
+
 
 // Get one subscriber
 router.get('/:id', (req, res) => {});
 
+
 // Create one subscriber
-router.post('/', (req, res) => {});
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({ // creating a variable that will be assigned to a new Subscriber from our model created earlier (name, subscribedChannel, and subscribeDate)
+    name: req.body.name,
+    subscribedChannel: req.body.subscribedChannel
+  });
+
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // Update one subscriber
 router.patch('/:id', (req, res) => {});
